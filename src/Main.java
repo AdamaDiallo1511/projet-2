fdf26dfecd06932c1db6cab14552664fe9e28c0c
jocker <mehmedbamba098@gmail.com>
2024-01-11 22:53:48 +0000

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ import java.util.HashSet;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Personne> personnes = new ArrayList<>();
-        personnes.add(new Etudiants("Kane", "Bamba", 2000, "jean.dupont@gmail.com", "1", Classe.L3));
-        personnes.add(new Etudiants("Durand", "Marie", 1999, "marie.durand@gmail.com", "2", Classe.M1));
-        personnes.add(new Etudiants("Martin", "Paul", 2001, "paul.martin@gmail.com", "3", Classe.M2));
-        personnes.add(new Professeur("Leroy", "Alice", 1980, "alice.leroy@gmail.com", new HashSet<>(Arrays.asList("JAVA", "PHP"))));
-        personnes.add(new Professeur("Lemoine", "Robert", 1975, "robert.lemoine@gmail.com", new HashSet<>(Arrays.asList("C#", "PHP"))));
-        personnes.add(new Professeur("Moreau", "Claire", 1985, "claire.moreau@gmail.com", new HashSet<>(Arrays.asList("JAVA", "C#"))));
+        personnes.add(new Etudiants("Kane", "Mouhamadou", 2000, "Mouhamadou.Kane@gmail.com", "1", Classe.L3));
+        personnes.add(new Etudiants("Kane", "Bamba", 1999, "Bamba.Kane@gmail.com", "2", Classe.M1));
+        personnes.add(new Etudiants("Kane", "Mehmed", 2001, "Mehmed.Kane@gmail.com", "3", Classe.M2));
+        personnes.add(new Professeur("Boy", "Mbour", 1980, "Boy.Mbour@gmail.com", new HashSet<>(Arrays.asList("JAVA", "PHP"))));
+        personnes.add(new Professeur("Java", "Lover", 1975, "Java.Lover@gmail.com", new HashSet<>(Arrays.asList("C#", "PHP"))));
+        personnes.add(new Professeur("Lo", "Dane", 1985, "Dane.Lo@gmail.com", new HashSet<>(Arrays.asList("JAVA", "C#"))));
 
         for (Personne personne : personnes) {
             System.out.println(personne);
